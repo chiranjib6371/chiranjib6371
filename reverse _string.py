c5aece81fcd24fd231df26c9f790e910149e0c86
chiranjib6371 <91051410+chiranjib6371@users.noreply.github.com>
2021-10-02 12:45:58 +0530

    Add files via upload

---
+++ reverse _string.py
@@ -0,0 +1,24 @@
+def reverseSting(text):
+    index = -1
+    for i in range(len(text) - 1, int(len(text) / 2), -1):
+        if text[i].isalpha():
+            temp = text[i]
+            while True:
+                index += 1
+                if text[index].isalpha():
+                    text[i] = text[index]
+                    text[index] = temp
+                    break
+    return text
+
+
+string = input("Enter some string:")
+print("string:", string)
+
+
+def reversedstring(param):
+    pass
+
+
+string = reversedstring(list(string))
+print("output string:", "".join(string))
